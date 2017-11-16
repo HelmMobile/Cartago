f57e15a7b471f277012cdd71d78599e746aebfa2
Miguel Berrocal Gómez <miguelberrocal.1991@gmail.com>
2017-11-16 18:25:29 +0100

    changed setup for version 0.3

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
         'cartago = cartago.cartago:main'
     ]
   },
-  version = '0.1',
+  version = '0.3',
   description = 'An automated framework installer using carthage',
   author = 'HELM S.C.P.',
   author_email = 'pypi@helm.cat',
   url = 'https://github.com/HelmMobile/Cartago',
-  download_url = 'https://github.com/HelmMobile/Cartago/archive/0.2.tar.gz',
+  download_url = 'https://github.com/HelmMobile/Cartago/archive/0.3.tar.gz',
   keywords = ['carthage', 'swift', 'xcode', 'ios', "framework", "script", "xcode project"],
   classifiers = [],
 )
